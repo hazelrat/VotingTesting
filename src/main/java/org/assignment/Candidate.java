616bc7cf7495e7d800515c4fffca6650d63fe7ba
Hazelrat <ric.whe.don@gmail.com>
2025-05-01 15:51:21 +0100

    Further branch edits

--- src/main/java/org/assignment/Candidate.java
+++ src/main/java/org/assignment/Candidate.java
@@ -5,129 +5,13 @@ Do not deviate from this object, as automated tests later in the project depend 
 
 package org.assignment;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Objects;
-
 public class Candidate {
-    @JsonProperty("name")
     String name;
-    @JsonProperty("votes")
     Integer votes;
 
     // Class constructor
-    @JsonCreator
-    public Candidate(
-            @JsonProperty("name") String canName,
-            @JsonProperty("votes") int canVotes) {
+    public Candidate(String canName, int canVotes) {
         this.name = canName;
         this.votes = canVotes;
-    }
-
-    /*
-    Populates the candidates json file.
-    Note that this is not additive! This will replace anything previous.
-    */
-    static void addCandidates(ArrayList<Candidate> candidate) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(new File("src/main/java/org/assignment/candidates.json"), candidate);
-    }
-
-    /*
-    Clears all contents of the json file.
-    */
-    static void clearCandidates() throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(new File("src/main/java/org/assignment/candidates.json"), null);
-    }
-
-    /*
-    Retrieves candidate list from json file.
-    */
-    static ArrayList<Candidate> retrieveCandidates() throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(new File("src/main/java/org/assignment/candidates.json"), new TypeReference<>() {});
-    }
-
-    /*
-    Adds a singular candidate to the existing list, beginning at zero votes.
-    */
-    static void addSingleCandidate(String candidateName) throws IOException {
-        ArrayList<Candidate> currentValues = retrieveCandidates();
-        Candidate newAddition = new Candidate(candidateName, 0);
-        currentValues.add(newAddition);
-        addCandidates(currentValues);
-    }
-
-    /*
-    Adds a single vote to a single candidate, as differentiated by their name.
-    */
-    static void addVote(String candidateName) throws IOException {
-        ArrayList<Candidate> currentValues = retrieveCandidates();
-        for (Candidate currentValue : currentValues) {
-            if (Objects.equals(currentValue.name, candidateName)) {
-                currentValue.votes += 1;
-            }
-        }
-        addCandidates(currentValues);
-    }
-
-    /*
-    Indiscriminately clears all stored votes.
-    */
-    static void clearVotes() throws IOException {
-        ArrayList<Candidate> currentValues = retrieveCandidates();
-        for (Candidate currentValue : currentValues) {
-            currentValue.votes = 0;
-        }
-        addCandidates(currentValues);
-    }
-
-    /*
-    Removes a single candidate from the list, as differentiated by their name.
-     */
-    static void removeCandidate(String candidateName) throws IOException {
-        ArrayList<Candidate> currentValues = retrieveCandidates();
-        currentValues.removeIf(currentValue -> Objects.equals(currentValue.name, candidateName));
-        addCandidates(currentValues);
-    }
-
-    /*
-    Calculates the winner of the election. This should work based on absolute majority.
-    If absolute majority via a single candidate is achieved, this should return an empty string.
-    */
-    static String calculateWinner() throws IOException {
-        ArrayList<Candidate> currentValues = retrieveCandidates();
-        Integer totalVotes = 0;
-
-        // Finding total number of votes.
-        for (Candidate candidate : currentValues) {
-            totalVotes += candidate.votes;
-        }
-
-        // Checking percentage.
-        ArrayList<Double> percentageValues = new ArrayList<>();
-        for (Candidate candidate : currentValues) {
-            Double percent = Double.valueOf(candidate.votes);
-            percentageValues.add(percent + totalVotes * 100);
-        }
-
-        // Returns no absolute majority by standard.
-        String returnValue = "No absolute majority detected.";
-
-        // Checks for an absolute majority.
-        for (int i = 0; i < percentageValues.size(); i++) {
-            if (percentageValues.get(i) >= 50) {
-                returnValue = "Absolute majority has been achieved by: " + currentValues.get(i).name + ", who has thereby won the election!";
-            }
-        }
-
-        return returnValue;
     }
 }
--- src/main/java/org/assignment/Logic.java
+++ src/main/java/org/assignment/Logic.java
@@ -6,9 +6,7 @@ These are automatically tested by the testing file in this project! Please use t
 
 package org.assignment;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Logic {
 
@@ -16,11 +14,7 @@ public class Logic {
     Should return total number of votes cast between candidates.
     */
     static Integer totalVoteCount(ArrayList<Candidate> candidates) {
-        int totalValue = 0;
-        for (Candidate candidate : candidates) {
-            totalValue += candidate.votes;
-        }
-        return totalValue;
+        return null;
     }
 
     /*
@@ -28,24 +22,14 @@ public class Logic {
     The format of the return value should be as follows: [minimum value of range] + " - " + [maximum value of range]
     */
     static String totalVoteRange(ArrayList<Candidate> candidates) {
-        ArrayList<Integer> rangeValues = new ArrayList<>();
-        for (Candidate candidate : candidates) {
-            rangeValues.add(candidate.votes);
-        }
-        return Collections.min(rangeValues) + " - " + Collections.max(rangeValues);
+        return null;
     }
 
     /*
     Should return the candidate with the most votes, as a Candidate object.
     */
     static Candidate voteMode(ArrayList<Candidate> candidates) {
-        Candidate returnCandidate = new Candidate("PLACEHOLDER", 0);
-        for (Candidate candidate : candidates) {
-            if (candidate.votes > returnCandidate.votes) {
-                returnCandidate = candidate;
-            }
-        }
-        return returnCandidate;
+        return null;
     }
 
     /*
@@ -55,25 +39,6 @@ public class Logic {
     Refer to the automated test for this function if you believe you have a formatting error.
     */
     static ArrayList<String> getPercentages(ArrayList<Candidate> candidates) {
-        // Using prior function to retrieve total number of votes. Necessary for percentage calculation.
-        Integer totalVotes = totalVoteCount(candidates);
-
-        // Populating list with percentages!
-        ArrayList<Double> percentageValues = new ArrayList<>();
-        for (Candidate candidate : candidates) {
-            Double percent = Double.valueOf(candidate.votes);
-            percentageValues.add(percent / totalVotes * 100);
-        }
-
-        // Formatting the percentages into a string.
-        DecimalFormat numberFormat = new DecimalFormat("#.00");
-        ArrayList<String> completeValues = new ArrayList<>();
-        for (int i = 0; i < candidates.size(); i++) {
-            Candidate candidateValue = candidates.get(i);
-            String nameValue = candidateValue.name;
-            completeValues.add(nameValue + " - " + numberFormat.format(percentageValues.get(i)) + "%");
-        }
-
-        return completeValues;
+        return null;
     }
 }
