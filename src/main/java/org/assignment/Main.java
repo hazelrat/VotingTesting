3ad40a3bb4c280ca9416b3151f26b0015f1b48a4
Hazelrat <ric.whe.don@gmail.com>
2025-05-01 15:57:42 +0100

    Added comment

--- src/main/java/org/assignment/Main.java
+++ src/main/java/org/assignment/Main.java
@@ -1,3 +1,12 @@
+/*
+This is the main class. You should be able to run the application from here once complete!
+You are expected to implement a voting system within this file, utilising the Candidate class and functions therein.
+Votes should be stored persistently, and the user should be able to lodge votes and edit the list of candidates.
+For higher marks, you may implement statistical functions already defined in the Logic class.
+You can utilise the LogicTests class to automatically test your work in the Logic class.
+Please refer to the brief for more comprehensive criteria!
+*/
+
 package org.assignment;
 
 import java.io.IOException;
